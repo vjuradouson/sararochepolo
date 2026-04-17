7e8f64f9fc5a1c44c1b886b382d3140e2226692f
Victor Jurado <victor.jurado@sivasdescalzo.com>
2026-04-17 08:28:29 +0200

    fix pet buddy project image

--- src/app/[locale]/(home)/components/ProjectsSection.tsx
+++ src/app/[locale]/(home)/components/ProjectsSection.tsx
@@ -84,7 +84,7 @@ export default function ProjectsSection() {
                     height={828}
                     sizes="(max-width: 768px) 100vw, 50vw"
                     quality={75}
-                    className="max-h-full max-w-full w-full h-auto object-contain drop-shadow-[15px_20px_10px_rgba(0,0,0,0.2)] my-5 mt-12 mb-12 md:mt-0 md:mb-0 mx-0 my-0 xl:scale-[1.3] xl:pl-[10%]"
+                    className="max-h-full max-w-full w-full h-auto object-contain drop-shadow-[15px_20px_10px_rgba(0,0,0,0.2)] my-5 mt-12 mb-12 md:mt-0 md:mb-0 mx-0 my-0 scale-[1.1] md:scale-[1] xl:scale-[1.3] xl:pl-[10%]"
                 />
             ),
         },
